19e5702aef34acad4ddce8efd3e1d8e7f4dd90b3
tory <33620824+tory121219@users.noreply.github.com>
2021-10-27 13:04:59 +0900

    Add files via upload

---
+++ js/10000hours.js
@@ -0,0 +1,5 @@
+// js file
+shareButton.addEventListener('click', copyUrl)
+openButton.addEventListener('click', openModal)
+closeButton.addEventListener('click', colseModal)
+startButton.addEventListener('click', calculator)
